cf777949c929e4b2cf7616a27dc763573651d76a
danielvidal01 <danielvidaldev@outlook.com>
2021-11-07 10:36:23 -0300

    chore(start): start project

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <pre>{{info}}</pre>
+  </div>
+</template>
+
+<script lang="ts">
+import {Vue , Component} from 'nuxt-property-decorator'
+@Component
+export default class Test extends Vue{
+  info:string= 'lalalal'
+}
+</script>
